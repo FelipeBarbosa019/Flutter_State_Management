5c9aba47db68b8d1fdc4d7aed647b2c788bdc57e
Felipe Barbosa <felipefb2@hotmail.com>
2023-02-21 20:16:40 -0300

    feat: add count

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,22 +11,58 @@ class HomePage extends StatefulWidget {
 class HomeState extends State<HomePage> {
   int count = 0;
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
+        actions: const [CustomSwitch()],
       ),
-      body: Center(
-        child: Switch(
-            value: AppController.instance.isDarkTheme,
-            onChanged: (value) {
-              AppController.instance.changeTheme();
-            }),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Quantidade adicionada: $count",
+              style: const TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  count = 0;
+                });
+              },
+              child: const Icon(Icons.restart_alt),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  const CustomSwitch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
     );
   }
 }
